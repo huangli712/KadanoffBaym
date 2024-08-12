e4a5bc0f8a76d5435c9a1409e9643998b6ac5226
Li Huang <lihuang.dmft@gmail.com>
2024-08-12 15:49:15 +0800

    fix types.jl

--- src/types.jl
+++ src/types.jl
@@ -7,3 +7,27 @@
 # Last modified: 2024/08/12
 #
 
+#=
+### *Derived Types*
+=#
+
+"""
+    Element{T}
+
+Type definition. A matrix.
+"""
+const Element{T} = Array{T,2}
+
+"""
+    MatArray{T}
+
+Type definition. A matrix of matrix.
+"""
+const MatArray{T} = Matrix{Element{T}}
+
+"""
+    VecArray{T}
+
+Type definition. A vector of matrix.
+"""
+const VecArray{T} = Vector{Element{T}}
