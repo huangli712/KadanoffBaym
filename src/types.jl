54051bbfb2b12eb202bb1fdab0c734f915d04cb4
Li Huang <lihuang.dmft@gmail.com>
2024-08-13 11:27:46 +0800

    fix types.jl

--- src/types.jl
+++ src/types.jl
@@ -5899,3 +5899,63 @@ function smul!(cfv::𝒻{S}, tstp::I64, α) where {S}
         smul!(cfv.mat, cα)
     end
 end
+
+"""
+    smul!(cff::Cf{S}, cfv::𝒻{S}, tstp::I64)
+
+Left multiply a `𝒻` with given weight (`Cf`) at given time
+step `tstp`.
+"""
+function smul!(cff::Cf{S}, cfv::𝒻{S}, tstp::I64) where {S}
+    @assert tstp == gettstp(cfv)
+    @assert tstp ≤ getsize(cff)
+    if tstp > 0
+        smul!(cff[tstp], cfv.ret)
+        smul!(cff[tstp], cfv.lmix)
+        smul!(cff, cfv.less)
+    else
+        smul!(cff[0], cfv.mat)
+    end
+end
+
+"""
+    smul!(cfv::𝒻{S}, cff::Cf{S}, tstp::I64)
+
+Right multiply a `𝒻` with given weight (`Cf`) at given time
+step `tstp`.
+"""
+function smul!(cfv::𝒻{S}, cff::Cf{S}, tstp::I64) where {S}
+    @assert tstp == gettstp(cfv)
+    @assert tstp ≤ getsize(cff)
+    if tstp > 0
+        smul!(cfv.ret, cff)
+        smul!(cfv.lmix, cff[0])
+        smul!(cfv.less, cff[tstp])
+    else
+        smul!(cfv.mat, cff[0])
+    end
+end
+
+#=
+### *𝒻* : *I/O*
+=#
+
+"""
+    read!(fname::AbstractString, cfv::𝒻{S})
+
+Read the contour Green's functions from given file.
+"""
+function read!(fname::AbstractString, cfv::𝒻{S}) where {S}
+end
+
+"""
+    write(fname::AbstractString, cfv::𝒻{S})
+
+Write the contour Green's functions to given file.
+"""
+function write(fname::AbstractString, cfv::𝒻{S}) where {S}
+end
+
+#=
+### *𝒻* : *Traits*
+=#
