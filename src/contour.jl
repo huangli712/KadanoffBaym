d99df5823378b5f57d641f90daece170a8bfa839
Li Huang <lihuang.dmft@gmail.com>
2025-10-15 16:20:53 +0800

    fix contour.jl and types.jl

---
+++ src/contour.jl
@@ -0,0 +1,740 @@
+
+
+#=
+*Remarks : Kadanoff-Baym Contour*
+
+We adopt an 𝐿-shaped `Kadanoff-Baym` contour ``\mathcal{C}`` with
+three branches:
+
+* ``\mathcal{C}_1``: ``0 \longrightarrow t_{\text{max}}``
+* ``\mathcal{C}_2``: ``t_{\text{max}} \longrightarrow 0``
+* ``\mathcal{C}_3``: ``0 \longrightarrow -i\beta``
+
+where ``t_{\text{max}}`` is the maximal time up to which one wants to
+let the system evolve and ``\beta`` is the inverse temperature
+(``\beta \equiv 1/T``).
+
+The expectation value of an observable ``\mathcal{O}`` measured at time
+``t`` is given by
+
+```math
+\begin{equation}
+\langle \mathcal{O}(t) \rangle =
+    \frac{\text{Tr}\left[
+                   \mathcal{T}_{\mathcal{C}}
+                   e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+                   \mathcal{O}(t)
+                   \right]}
+         {\text{Tr}\left[
+                   \mathcal{T}_{\mathcal{C}}
+                   e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+         \right]},
+\end{equation}
+```
+
+where ``\mathcal{T}_{\mathcal{C}}`` is a contour-ordering operator
+that arranges operators on the contour ``\mathcal{C}`` in the order
+``0 \to t_{\text{max}} \to 0 \to -i\beta``, while ``\mathcal{T}_{\tau}``
+is the time-ordering operator only on the imaginary-time axis.
+
+The contour-ordered formalism reveals its full power when it is applied
+to higher-order correlation functions,
+
+```math
+\begin{equation}
+\langle \mathcal{T}_{\mathcal{C}} \mathcal{A}(t) \mathcal{B}(t') \rangle
+    =
+    \frac{1}{Z} \text{Tr}
+    \left[
+        \mathcal{T}_{\mathcal{C}}
+        e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+        \mathcal{A}(t) \mathcal{B}(t')
+    \right].
+\end{equation}
+```
+
+Here ``\mathcal{A}`` and ``\mathcal{B}`` are combinations of particle's
+creation and annihilation operators. We call them ''fermionic'' if they
+contain odd number of fermion's creation or annihilation operators, and
+''bosonic'' otherwise. In this expression, ``t`` and ``t'`` can lie
+anywhere on ``\mathcal{C}``, and the contour-ordered product of two
+operators ``\mathcal{A}`` and ``\mathcal{B}`` is defined as
+
+```math
+\begin{equation}
+\mathcal{T}_{\mathcal{C}} \mathcal{A}(t) \mathcal{B}(t')
+    = \theta_{\mathcal{C}}(t,t') \mathcal{A}(t) \mathcal{B}(t')
+    \pm \theta_{\mathcal{C}}(t',t) \mathcal{B}(t') \mathcal{A}(t),
+\end{equation}
+```
+
+where ``\theta_{\mathcal{C}}(t,t') = 1`` when ``t'`` comes earlier than
+``t`` in the contour ordering (denoted by ``t \succ t'``) and 0 otherwise
+(``t \prec t'``). The sign ``\pm`` is taken to be minus when the operators
+``\mathcal{A}`` and ``\mathcal{B}`` are both fermionic and plus otherwise.
+
+---
+
+*Remarks : Contour-ordered Green's Functions*
+
+In the many-body theories, single-particle Green's functions are the
+fundamental objects. They describe single-particle excitations as well
+as statistical distributions of particles, and play a central role in
+the formulation of nonequilibrium dynamical mean-field theory. We define
+the nonequilibrium Green's function as the contour-ordered expectation
+value,
+
+```math
+\begin{equation}
+G(t,t') \equiv
+    -i \langle \mathcal{T}_{\mathcal{C}} c(t) c^{\dagger}(t') \rangle,
+\end{equation}
+```
+
+where ``c^{\dagger}(c)`` is a creation (annihilation) operators of
+particles, and ``t,\ t' \in \mathcal{C}``. For simplicity, spin and
+orbital indices associated with the operators are not shown. Because
+of the three branches, on which the time arguments ``t`` and ``t'``
+can lie, the Green's function has ``3 \times 3 = 9`` components:
+
+```math
+\begin{equation}
+G(t,t') \equiv G_{ij}(t,t'),
+\end{equation}
+```
+
+where ``t \in \mathcal{C}_i``, ``t' \in \mathcal{C}_j``, and
+``i,\ j = 1,\ 2,\ 3``. Conventionally, we can express them in a
+``3 \times 3`` matrix form
+
+```math
+\begin{equation}
+\hat{G} =
+\begin{pmatrix}
+G_{11} & G_{12} & G_{13} \\
+G_{21} & G_{22} & G_{23} \\
+G_{31} & G_{32} & G_{33} \\
+\end{pmatrix}.
+\end{equation}
+```
+
+In general, one can shift the operator with the largest real-time
+argument from ``\mathcal{C}_1`` to ``\mathcal{C}_2`` (and vice versa),
+because the time evolution along ``\mathcal{C}_1`` and ``\mathcal{C}_2``
+to the right of that operator cancels. This kind of redundancy implies
+the following relations among the components of the above matrix:
+
+```math
+\begin{equation}
+G_{11}(t,t') = G_{12}(t,t'),\ \text{for}\ t \le t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{11}(t,t') = G_{21}(t,t'),\ \text{for}\ t > t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{22}(t,t') = G_{21}(t,t'),\ \text{for}\ t < t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{22}(t,t') = G_{12}(t,t'),\ \text{for}\ t \ge t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{13}(t,\tau') = G_{23}(t,\tau'),
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{31}(\tau,t') = G_{32}(\tau,t').
+\end{equation}
+```
+
+The above equations can be summarized as
+
+```math
+\begin{equation}
+G_{11} + G_{22} = G_{12} + G_{21}.
+\end{equation}
+```
+
+These equations thus allow one to eliminate three components out of
+nine in the nonequilibrium Green's function. To this end, we introduce
+six linearly independent physical Green's functions, namely the retarded
+(``G^{R}``), advanced (``G^{A}``), Keldysh (``G^{K}``), left-mixing
+(``G^{\rceil}``), right-mixing (``G^{\lceil}``), and Matsubara Green's
+functions (``G^{M}``). Their definitions and relevant properties will
+be given in the following remarks if needed.
+=#
+
+#=
+### *Cn* : *Struct*
+=#
+
+"""
+    Cn
+
+𝐿-shape `Kadanoff-Baym` contour. It includes the following members:
+
+* ntime -> Number of time slices in real time axis [0, 𝑡max].
+* ntau -> Number of time slices in imaginary time axis [0, β].
+* ndim1 -> Size of operators that stored in the contour.
+* ndim2 -> Size of operators that stored in the contour.
+* tmax -> Maximum 𝑡.
+* beta -> β, inverse temperature.
+* dt -> δ𝑡, time step in real axis.
+* dtau -> δτ, time step in imaginary axis.
+
+See also: [`CnAbstractType`](@ref).
+"""
+mutable struct Cn <: CnAbstractType
+    ntime :: I64
+    ntau  :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    tmax  :: F64
+    beta  :: F64
+    dt    :: F64
+    dtau  :: F64
+end
+
+#=
+### *Cn* : *Constructors*
+=#
+
+"""
+    Cn(ntime::I64, ntau::I64, ndim1::I64,
+       ndim2::I64, tmax::F64, beta::F64)
+
+Constructor. Create a general 𝐿-shape `Kadanoff-Baym` contour.
+"""
+function Cn(ntime::I64, ntau::I64, ndim1::I64,
+            ndim2::I64, tmax::F64, beta::F64)
+    # Sanity check
+    @assert ntime ≥ 2
+    @assert ntau  ≥ 2
+    @assert ndim1 ≥ 1
+    @assert ndim2 ≥ 1
+    @assert tmax  > 0.0
+    @assert beta  > 0.0
+
+    # Evaluate `dt` and `dtau`
+    dt = tmax / ( ntime - 1 )
+    dtau = beta / ( ntau - 1 )
+
+    # Call the default constructor
+    Cn(ntime, ntau, ndim1, ndim2, tmax, beta, dt, dtau)
+end
+
+"""
+    Cn(ndim1::I64, ndim2::I64, tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+general matrix system.
+"""
+function Cn(ndim1::I64, ndim2::I64, tmax::F64, beta::F64)
+    ntime = 201
+    ntau = 1001
+    Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+end
+
+"""
+    Cn(ndim1::I64, tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+special square matrix system.
+"""
+function Cn(ndim1::I64, tmax::F64, beta::F64)
+    ndim2 = ndim1
+    Cn(ndim1, ndim2, tmax, beta)
+end
+
+"""
+    Cn(tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+special single band system.
+"""
+function Cn(tmax::F64, beta::F64)
+    ndim1 = 1
+    ndim2 = 1
+    Cn(ndim1, ndim2, tmax, beta)
+end
+
+#=
+### *Cn* : *Properties*
+=#
+
+"""
+    getdims(C::Cn)
+
+Return the dimensional parameters of contour.
+
+See also: [`Cn`](@ref).
+"""
+function getdims(C::Cn)
+    return (C.ndim1, C.ndim2)
+end
+
+"""
+    equaldims(C::Cn)
+
+Return whether the dimensional parameters are equal.
+
+See also: [`Cn`](@ref).
+"""
+function equaldims(C::Cn)
+    return C.ndim1 == C.ndim2
+end
+
+#=
+### *Cn* : *Operations*
+=#
+
+"""
+    refresh!(C::Cn)
+
+Update the `dt` and `dtau` parameters of contour.
+
+See also: [`Cn`](@ref).
+"""
+function refresh!(C::Cn)
+    # Sanity check
+    @assert C.ntime ≥ 2
+    @assert C.ntau ≥ 2
+
+    # Evaluate `dt` and `dtau` again
+    C.dt = C.tmax / ( C.ntime - 1 )
+    C.dtau = C.beta / ( C.ntau - 1 )
+end
+
+#=
+### *Cn* : *Traits*
+=#
+
+"""
+    Base.show(io::IO, C::Cn)
+
+Display `Cn` struct on the terminal.
+
+See also: [`Cn`](@ref).
+"""
+function Base.show(io::IO, C::Cn)
+    println("ntime : ", C.ntime)
+    println("ntau  : ", C.ntau )
+    println("ndim1 : ", C.ndim1)
+    println("ndim2 : ", C.ndim2)
+    println("tmax  : ", C.tmax )
+    println("beta  : ", C.beta )
+    println("dt    : ", C.dt   )
+    println("dtau  : ", C.dtau )
+end
+
+#=
+*Remarks : Contour-based Functions*
+
+It is a general matrix-valued function defined at the `Kadanoff-Baym`
+contour:
+
+```math
+\begin{equation}
+f_{\mathcal{C}} = f(t),
+\end{equation}
+```
+
+where ``t \in \mathcal{C}_1 \cup \mathcal{C}_2 \cup \mathcal{C}_3``.
+=#
+
+#=
+### *Cf* : *Struct*
+=#
+
+"""
+    Cf{T}
+
+It is a square-matrix-valued or rectangle-matrix-valued function of time.
+
+See also: [`ℱ`](@ref), [`𝒻`](@ref).
+"""
+mutable struct Cf{T} <: CnAbstractFunction{T}
+    ntime :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    data  :: VecArray{T}
+end
+
+#=
+### *Cf* : *Constructors*
+=#
+
+"""
+    Cf(ntime::I64, ndim1::I64, ndim2::I64, v::T)
+
+Constructor. All the matrix elements are set to be `v`.
+"""
+function Cf(ntime::I64, ndim1::I64, ndim2::I64, v::T) where {T}
+    # Sanity check
+    #
+    # If `ntime = 0`, it means that the system stays at the equilibrium
+    # state and this function is defined at the Matsubara axis only.
+    @assert ntime ≥ 0
+    @assert ndim1 ≥ 1
+    @assert ndim2 ≥ 1
+
+    # Create Element{T}
+    element = fill(v, ndim1, ndim2)
+
+    # Create VecArray{T}, whose size is indeed (ntime + 1,).
+    #
+    # Be careful, data[end] is the value of the function on the
+    # Matsubara axis (initial state), while data[1:ntime] is for
+    # the time evolution part.
+    data = VecArray{T}(undef, ntime + 1)
+    for i = 1:ntime + 1
+        data[i] = copy(element)
+    end
+
+    # Call the default constructor
+    Cf(ntime, ndim1, ndim2, data)
+end
+
+"""
+    Cf(ntime::I64, ndim1::I64, ndim2::I64)
+
+Constructor. All the matrix elements are set to be complex zero.
+"""
+function Cf(ntime::I64, ndim1::I64, ndim2::I64)
+    Cf(ntime, ndim1, ndim2, zero(C64))
+end
+
+"""
+    Cf(ntime::I64, ndim1::I64)
+
+Constructor. All the matrix elements are set to be complex zero.
+"""
+function Cf(ntime::I64, ndim1::I64)
+    Cf(ntime, ndim1, ndim1, zero(C64))
+end
+
+"""
+    Cf(ntime::I64, x::Element{T})
+
+Constructor. The matrix is initialized by `x`.
+"""
+function Cf(ntime::I64, x::Element{T}) where {T}
+    # Sanity check
+    @assert ntime ≥ 0
+
+    ndim1, ndim2 = size(x)
+    data = VecArray{T}(undef, ntime + 1)
+    for i = 1:ntime + 1
+        data[i] = copy(x)
+    end
+
+    # Call the default constructor
+    Cf(ntime, ndim1, ndim2, data)
+end
+
+"""
+    Cf(C::Cn, x::Element{T})
+
+Constructor. The matrix is initialized by `x`.
+"""
+function Cf(C::Cn, x::Element{T}) where {T}
+    # Sanity check
+    @assert getdims(C) == size(x)
+
+    # Create VecArray{T}, whose size is indeed (ntime + 1,).
+    data = VecArray{T}(undef, C.ntime + 1)
+    for i = 1:C.ntime + 1
+        data[i] = copy(x)
+    end
+
+    # Call the default constructor
+    Cf(C.ntime, C.ndim1, C.ndim2, data)
+end
+
+"""
+    Cf(C::Cn, v::T)
+
+Constructor. All the matrix elements are set to be `v`.
+"""
+function Cf(C::Cn, v::T) where {T}
+    Cf(C.ntime, C.ndim1, C.ndim2, v)
+end
+
+"""
+    Cf(C::Cn)
+
+Constructor. All the matrix elements are set to be complex zero.
+"""
+function Cf(C::Cn)
+    Cf(C.ntime, C.ndim1, C.ndim2, zero(C64))
+end
+
+#=
+### *Cf* : *Properties*
+=#
+
+"""
+    getdims(cf::Cf{T})
+
+Return the dimensional parameters of contour function.
+
+See also: [`Cf`](@ref).
+"""
+function getdims(cf::Cf{T}) where {T}
+    return (cf.ndim1, cf.ndim2)
+end
+
+"""
+    getsize(cf::Cf{T})
+
+Return the nominal size of contour function, i.e `ntime`. Actually, the
+real size of contour function should be `ntime + 1`.
+
+See also: [`Cf`](@ref).
+"""
+function getsize(cf::Cf{T}) where {T}
+    return cf.ntime
+end
+
+"""
+    equaldims(cf::Cf{T})
+
+Return whether the dimensional parameters are equal.
+
+See also: [`Cf`](@ref).
+"""
+function equaldims(cf::Cf{T}) where {T}
+    return cf.ndim1 == cf.ndim2
+end
+
+"""
+    iscompatible(cf1::Cf{T}, cf2::Cf{T})
+
+Judge whether two `Cf` objects are compatible.
+"""
+function iscompatible(cf1::Cf{T}, cf2::Cf{T}) where {T}
+    getsize(cf1) == getsize(cf2) &&
+    getdims(cf1) == getdims(cf2)
+end
+
+"""
+    iscompatible(C::Cn, cf::Cf{T})
+
+Judge whether `C` (which is a `Cn` object) is compatible with `cf`
+(which is a `Cf{T}` object).
+"""
+function iscompatible(C::Cn, cf::Cf{T}) where {T}
+    C.ntime == getsize(cf) &&
+    getdims(C) == getdims(cf)
+end
+
+"""
+    iscompatible(cf::Cf{T}, C::Cn)
+
+Judge whether `C` (which is a `Cn` object) is compatible with `cf`
+(which is a `Cf{T}` object).
+"""
+iscompatible(cf::Cf{T}, C::Cn) where {T} = iscompatible(C, cf)
+
+#=
+### *Cf* : *Indexing*
+=#
+
+"""
+    Base.getindex(cf::Cf{T}, i::I64)
+
+Visit the element stored in `Cf` object. If `i = 0`, it returns
+the element at Matsubara axis. On the other hand, if `i > 0`, it will
+return elements at real time axis.
+"""
+function Base.getindex(cf::Cf{T}, i::I64) where {T}
+    # Sanity check
+    @assert 0 ≤ i ≤ cf.ntime
+
+    # Return 𝑓(𝑡ᵢ)
+    if i == 0 # Matsubara axis
+        cf.data[end]
+    else # Real time axis
+        cf.data[i]
+    end
+end
+
+"""
+    Base.setindex!(cf::Cf{T}, x::Element{T}, i::I64)
+
+Setup the element in `Cf` object. If `i = 0`, it will setup the
+element at Matsubara axis to `x`. On the other hand, if `i > 0`, it
+will setup elements at real time axis.
+"""
+function Base.setindex!(cf::Cf{T}, x::Element{T}, i::I64) where {T}
+    # Sanity check
+    @assert size(x) == getdims(cf)
+    @assert 0 ≤ i ≤ cf.ntime
+
+    # 𝑓(𝑡ᵢ) = x
+    if i == 0 # Matsubara axis
+        cf.data[end] = copy(x)
+    else # Real time axis
+        cf.data[i] = copy(x)
+    end
+end
+
+"""
+    Base.setindex!(cf::Cf{T}, v::T, i::I64)
+
+Setup the element in `Cf` object. If `i = 0`, it will setup the
+element at Matsubara axis to `v`. On the other hand, if `i > 0`, it
+will setup elements at real time axis. Here, `v` should be a scalar
+number.
+"""
+function Base.setindex!(cf::Cf{T}, v::T, i::I64) where {T}
+    # Sanity check
+    @assert 0 ≤ i ≤ cf.ntime
+
+    # 𝑓(𝑡ᵢ) .= v
+    if i == 0 # Matsubara axis
+        fill!(cf.data[end], v)
+    else # Real time axis
+        fill!(cf.data[i], v)
+    end
+end
+
+#=
+### *Cf* : *Operations*
+=#
+
+"""
+    memset!(cf::Cf{T}, x)
+
+Reset all the matrix elements of `cf` to `x`. `x` should be a
+scalar number.
+"""
+function memset!(cf::Cf{T}, x) where {T}
+    cx = convert(T, x)
+    for i = 1:cf.ntime + 1
+        fill!(cf.data[i], cx)
+    end
+end
+
+"""
+    zeros!(cf::Cf{T})
+
+Reset all the matrix elements of `cf` to `zero`.
+"""
+zeros!(cf::Cf{T}) where {T} = memset!(cf, zero(T))
+
+"""
+    memcpy!(src::Cf{T}, dst::Cf{T})
+
+Copy all the matrix elements from `src` to `dst`.
+"""
+function memcpy!(src::Cf{T}, dst::Cf{T}) where {T}
+    @assert iscompatible(src, dst)
+    @. dst.data = copy(src.data)
+end
+
+"""
+    incr!(cf1::Cf{T}, cf2::Cf{T}, α::T)
+
+Add a `Cf` with given weight (`α`) to another `Cf`. Finally,
+`cf1` will be changed.
+"""
+function incr!(cf1::Cf{T}, cf2::Cf{T}, α::T) where {T}
+    @assert iscompatible(cf1, cf2)
+    for i = 1:cf1.ntime + 1
+        @. cf1.data[i] = cf1.data[i] + cf2.data[i] * α
+    end
+end
+
+"""
+    smul!(cf::Cf{T}, α::T)
+
+Multiply a `Cf` with given weight (`α`).
+"""
+function smul!(cf::Cf{T}, α::T) where {T}
+    for i = 1:cf.ntime + 1
+        @. cf.data[i] = cf.data[i] * α
+    end
+end
+
+"""
+    smul!(x::Element{T}, cf::Cf{T})
+
+Left multiply a `Cf` with given weight (`x`).
+"""
+function smul!(x::Element{T}, cf::Cf{T}) where {T}
+    for i = 1:cf.ntime + 1
+        cf.data[i] = x * cf.data[i]
+    end
+end
+
+"""
+    smul!(cf::Cf{T}, x::Element{T})
+
+Right multiply a `Cf` with given weight (`x`).
+"""
+function smul!(cf::Cf{T}, x::Element{T}) where {T}
+    for i = 1:cf.ntime + 1
+        cf.data[i] = cf.data[i] * x
+    end
+end
+
+#=
+### *Cf* : *Traits*
+=#
+
+"""
+    Base.:+(cf1::Cf{T}, cf2::Cf{T})
+
+Operation `+` for two `Cf` objects.
+"""
+function Base.:+(cf1::Cf{T}, cf2::Cf{T}) where {T}
+    # Sanity check
+    @assert getsize(cf1) == getsize(cf2)
+    @assert getdims(cf1) == getdims(cf2)
+
+    Cf(cf1.ntime, cf1.ndim1, cf1.ndim2, cf1.data + cf2.data)
+end
+
+"""
+    Base.:-(cf1::Cf{T}, cf2::Cf{T})
+
+Operation `-` for two `Cf` objects.
+"""
+function Base.:-(cf1::Cf{T}, cf2::Cf{T}) where {T}
+    # Sanity check
+    @assert getsize(cf1) == getsize(cf2)
+    @assert getdims(cf1) == getdims(cf2)
+
+    Cf(cf1.ntime, cf1.ndim1, cf1.ndim2, cf1.data - cf2.data)
+end
+
+"""
+    Base.:*(cf::Cf{T}, x)
+
+Operation `*` for a `Cf` object and a scalar value.
+"""
+function Base.:*(cf::Cf{T}, x) where {T}
+    cx = convert(T, x)
+    Cf(cf.ntime, cf.ndim1, cf.ndim2, cf.data * cx)
+end
+
+"""
+    Base.:*(x, cf::Cf{T})
+
+Operation `*` for a scalar value and a `Cf` object.
+"""
+Base.:*(x, cf::Cf{T}) where {T} = Base.:*(cf, x)
