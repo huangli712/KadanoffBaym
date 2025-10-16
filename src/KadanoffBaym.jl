2972d0e7cd60a1b46cca07512bb7c1aedda3f9cc
Li Huang <lihuang.dmft@gmail.com>
2025-10-16 16:21:48 +0800

    fix KadanoffBaym.jl

--- src/KadanoffBaym.jl
+++ src/KadanoffBaym.jl
@@ -148,6 +148,68 @@ export CnAbstractFunction
 #
 export subtypetree
 
+#=
+### *Includes And Exports* : *util.jl*
+=#
+
+#=
+*Summary* :
+
+To provide some useful utility macros and functions. They can be used
+to colorize the output strings, query the environments, and parse the
+input strings, etc.
+
+*Members* :
+
+```text
+@cswitch      -> C-style switch.
+@time_call    -> Evaluate a function call and print the elapsed time.
+@pcs          -> Print colorful strings.
+#
+require       -> Check julia envirnoment.
+setup_args    -> Setup ARGS manually.
+query_args    -> Query program's arguments.
+trace_error   -> Write exceptions or errors to terminal or external file.
+catch_error   -> Catch the thrown exceptions or errors.
+welcome       -> Print welcome message.
+overview      -> Print runtime information of KadanoffBaym.
+goodbye       -> Say goodbye.
+sorry         -> Say sorry.
+prompt        -> Print some messages or logs to the output devices.
+line_to_array -> Convert a line to a string array.
+```
+=#
+
+#
+include("util.jl")
+#
+export @cswitch
+export @time_call
+export @pcs
+#
+export require
+export setup_args
+export query_args
+export trace_error
+export catch_error
+export welcome
+export overview
+export goodbye
+export sorry
+export prompt
+export line_to_array
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -198,41 +260,7 @@ export smul!
 export read!
 export write
 
-#=
-### *Includes And Exports* : *util.jl*
-=#
 
-#=
-*Summary* :
-
-To provide some useful utility macros and functions. They can be used
-to colorize the output strings, query the environments, and parse the
-input strings, etc.
-
-*Members* :
-
-```text
-@cswitch      -> C-style switch.
-@time_call    -> Evaluate a function call and print the elapsed time.
-@pcs          -> Print colorful strings.
-#
-sorry         -> Say sorry.
-prompt        -> Print some messages or logs to the output devices.
-line_to_array -> Convert a line to a string array.
-```
-=#
-
-#
-include("util.jl")
-#
-export @cswitch
-export @time_call
-export @pcs
-#
-export welcome
-export sorry
-export prompt
-export line_to_array
 
 #=
 ### *Includes and Exports* : *weights.jl*
