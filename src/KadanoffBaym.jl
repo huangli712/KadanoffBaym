5b094e90f4115ad266b7d75bbf8cbbc915bc336b
Li Huang <lihuang.dmft@gmail.com>
2025-10-16 15:41:42 +0800

    fix comments

--- src/KadanoffBaym.jl
+++ src/KadanoffBaym.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/10/15
+# Last modified: 2025/10/16
 #
 
 """
@@ -28,33 +28,6 @@ Simulation package) code, which was developed and maintained by Martin
 Eckstein *et al*. Actually, it can be regarded as a replacement of the
 `NESSi` package for those peoples who don't like or aren't familiar
 with `C++`.
-
-### References
-
-[`NESSi`]
-Martin Eckstein, *et al.*,
-NESSi: The Non-Equilibrium Systems Simulation package,
-*Computer Physics Communications* **257**, 107484 (2020)
-
-[`REVIEW`]
-Philipp Werner, *et al.*,
-Nonequilibrium dynamical mean-field theory and its applications,
-*Reviews of Modern Physics* **86**, 779 (2014)
-
-[`MABOOK`]
-Johan de Villiers,
-Mathematics of Approximation,
-*Atlantis Press* (2012)
-
-[`MATABLE`]
-Dan Zwillinger (editor),
-CRC Standard Mathematical Tables and Formulas (33rd edition),
-*CRC Press* (*Taylor & Francis Group*) (2018)
-
-[`QUADRATURE`]
-Ruben J. Espinosa-Maldonado and George D. Byrne,
-On the Convergence of Quadrature Formulas,
-*SIAM J. Numer. Anal.* **8**, 110 (1971)
 """
 module KadanoffBaym
 
