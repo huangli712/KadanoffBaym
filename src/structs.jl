5910899a1190c4fbbc6bebe9dfaf059468872506
Li Huang <lihuang.dmft@gmail.com>
2025-10-16 17:49:15 +0800

    fix contour.jl

--- src/structs.jl
+++ src/structs.jl
@@ -4,8 +4,279 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/10/15
+# Last modified: 2025/10/16
 #
+
+#=
+*Remarks : Kadanoff-Baym Contour*
+
+We adopt an 𝐿-shaped `Kadanoff-Baym` contour ``\mathcal{C}`` with
+three branches:
+
+* ``\mathcal{C}_1``: ``0 \longrightarrow t_{\text{max}}``
+* ``\mathcal{C}_2``: ``t_{\text{max}} \longrightarrow 0``
+* ``\mathcal{C}_3``: ``0 \longrightarrow -i\beta``
+
+where ``t_{\text{max}}`` is the maximal time up to which one wants to
+let the system evolve and ``\beta`` is the inverse temperature
+(``\beta \equiv 1/T``).
+
+The expectation value of an observable ``\mathcal{O}`` measured at time
+``t`` is given by
+
+```math
+\begin{equation}
+\langle \mathcal{O}(t) \rangle =
+    \frac{\text{Tr}\left[
+                   \mathcal{T}_{\mathcal{C}}
+                   e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+                   \mathcal{O}(t)
+                   \right]}
+         {\text{Tr}\left[
+                   \mathcal{T}_{\mathcal{C}}
+                   e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+         \right]},
+\end{equation}
+```
+
+where ``\mathcal{T}_{\mathcal{C}}`` is a contour-ordering operator
+that arranges operators on the contour ``\mathcal{C}`` in the order
+``0 \to t_{\text{max}} \to 0 \to -i\beta``, while ``\mathcal{T}_{\tau}``
+is the time-ordering operator only on the imaginary-time axis.
+
+The contour-ordered formalism reveals its full power when it is applied
+to higher-order correlation functions,
+
+```math
+\begin{equation}
+\langle \mathcal{T}_{\mathcal{C}} \mathcal{A}(t) \mathcal{B}(t') \rangle
+    =
+    \frac{1}{Z} \text{Tr}
+    \left[
+        \mathcal{T}_{\mathcal{C}}
+        e^{-i\int_{\mathcal{C}} d\bar{t} \mathcal{H}(\bar{t})}
+        \mathcal{A}(t) \mathcal{B}(t')
+    \right].
+\end{equation}
+```
+
+Here ``\mathcal{A}`` and ``\mathcal{B}`` are combinations of particle's
+creation and annihilation operators. We call them ''fermionic'' if they
+contain odd number of fermion's creation or annihilation operators, and
+''bosonic'' otherwise. In this expression, ``t`` and ``t'`` can lie
+anywhere on ``\mathcal{C}``, and the contour-ordered product of two
+operators ``\mathcal{A}`` and ``\mathcal{B}`` is defined as
+
+```math
+\begin{equation}
+\mathcal{T}_{\mathcal{C}} \mathcal{A}(t) \mathcal{B}(t')
+    = \theta_{\mathcal{C}}(t,t') \mathcal{A}(t) \mathcal{B}(t')
+    \pm \theta_{\mathcal{C}}(t',t) \mathcal{B}(t') \mathcal{A}(t),
+\end{equation}
+```
+
+where ``\theta_{\mathcal{C}}(t,t') = 1`` when ``t'`` comes earlier than
+``t`` in the contour ordering (denoted by ``t \succ t'``) and 0 otherwise
+(``t \prec t'``). The sign ``\pm`` is taken to be minus when the operators
+``\mathcal{A}`` and ``\mathcal{B}`` are both fermionic and plus otherwise.
+
+---
+
+*Remarks : Contour-ordered Green's Functions*
+
+In the many-body theories, single-particle Green's functions are the
+fundamental objects. They describe single-particle excitations as well
+as statistical distributions of particles, and play a central role in
+the formulation of nonequilibrium dynamical mean-field theory. We define
+the nonequilibrium Green's function as the contour-ordered expectation
+value,
+
+```math
+\begin{equation}
+G(t,t') \equiv
+    -i \langle \mathcal{T}_{\mathcal{C}} c(t) c^{\dagger}(t') \rangle,
+\end{equation}
+```
+
+where ``c^{\dagger}(c)`` is a creation (annihilation) operators of
+particles, and ``t,\ t' \in \mathcal{C}``. For simplicity, spin and
+orbital indices associated with the operators are not shown. Because
+of the three branches, on which the time arguments ``t`` and ``t'``
+can lie, the Green's function has ``3 \times 3 = 9`` components:
+
+```math
+\begin{equation}
+G(t,t') \equiv G_{ij}(t,t'),
+\end{equation}
+```
+
+where ``t \in \mathcal{C}_i``, ``t' \in \mathcal{C}_j``, and
+``i,\ j = 1,\ 2,\ 3``. Conventionally, we can express them in a
+``3 \times 3`` matrix form
+
+```math
+\begin{equation}
+\hat{G} =
+\begin{pmatrix}
+G_{11} & G_{12} & G_{13} \\
+G_{21} & G_{22} & G_{23} \\
+G_{31} & G_{32} & G_{33} \\
+\end{pmatrix}.
+\end{equation}
+```
+
+In general, one can shift the operator with the largest real-time
+argument from ``\mathcal{C}_1`` to ``\mathcal{C}_2`` (and vice versa),
+because the time evolution along ``\mathcal{C}_1`` and ``\mathcal{C}_2``
+to the right of that operator cancels. This kind of redundancy implies
+the following relations among the components of the above matrix:
+
+```math
+\begin{equation}
+G_{11}(t,t') = G_{12}(t,t'),\ \text{for}\ t \le t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{11}(t,t') = G_{21}(t,t'),\ \text{for}\ t > t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{22}(t,t') = G_{21}(t,t'),\ \text{for}\ t < t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{22}(t,t') = G_{12}(t,t'),\ \text{for}\ t \ge t',
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{13}(t,\tau') = G_{23}(t,\tau'),
+\end{equation}
+```
+
+```math
+\begin{equation}
+G_{31}(\tau,t') = G_{32}(\tau,t').
+\end{equation}
+```
+
+The above equations can be summarized as
+
+```math
+\begin{equation}
+G_{11} + G_{22} = G_{12} + G_{21}.
+\end{equation}
+```
+
+These equations thus allow one to eliminate three components out of
+nine in the nonequilibrium Green's function. To this end, we introduce
+six linearly independent physical Green's functions, namely the retarded
+(``G^{R}``), advanced (``G^{A}``), Keldysh (``G^{K}``), left-mixing
+(``G^{\rceil}``), right-mixing (``G^{\lceil}``), and Matsubara Green's
+functions (``G^{M}``). Their definitions and relevant properties will
+be given in the following remarks if needed.
+=#
+
+#=
+### *Cn* : *Struct*
+=#
+
+"""
+    Cn
+
+𝐿-shape `Kadanoff-Baym` contour. It includes the following members:
+
+* ntime -> Number of time slices in real time axis [0, 𝑡max].
+* ntau -> Number of time slices in imaginary time axis [0, β].
+* ndim1 -> Size of operators that stored in the contour.
+* ndim2 -> Size of operators that stored in the contour.
+* tmax -> Maximum 𝑡.
+* beta -> β, inverse temperature.
+* dt -> δ𝑡, time step in real time axis.
+* dtau -> δτ, time step in imaginary time axis.
+
+See also: [`CnAbstractType`](@ref).
+"""
+mutable struct Cn <: CnAbstractType
+    ntime :: I64
+    ntau  :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    tmax  :: F64
+    beta  :: F64
+    dt    :: F64
+    dtau  :: F64
+end
+
+#=
+### *Cn* : *Constructors*
+=#
+
+"""
+    Cn(ntime::I64, ntau::I64, ndim1::I64,
+       ndim2::I64, tmax::F64, beta::F64)
+
+Constructor. Create a general 𝐿-shape `Kadanoff-Baym` contour.
+"""
+function Cn(ntime::I64, ntau::I64, ndim1::I64,
+            ndim2::I64, tmax::F64, beta::F64)
+    # Sanity check
+    @assert ntime ≥ 2
+    @assert ntau  ≥ 2
+    @assert ndim1 ≥ 1
+    @assert ndim2 ≥ 1
+    @assert tmax  > 0.0
+    @assert beta  > 0.0
+
+    # Evaluate `dt` and `dtau`
+    dt = tmax / ( ntime - 1 )
+    dtau = beta / ( ntau - 1 )
+
+    # Call the default constructor
+    Cn(ntime, ntau, ndim1, ndim2, tmax, beta, dt, dtau)
+end
+
+"""
+    Cn(ndim1::I64, ndim2::I64, tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+general matrix system.
+"""
+function Cn(ndim1::I64, ndim2::I64, tmax::F64, beta::F64)
+    ntime = 201
+    ntau = 1001
+    Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+end
+
+"""
+    Cn(ndim1::I64, tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+special square matrix system.
+"""
+function Cn(ndim1::I64, tmax::F64, beta::F64)
+    ndim2 = ndim1
+    Cn(ndim1, ndim2, tmax, beta)
+end
+
+"""
+    Cn(tmax::F64, beta::F64)
+
+Constructor. With default `ntime` (= 201) and `ntau` (= 1001). For
+special single band system.
+"""
+function Cn(tmax::F64, beta::F64)
+    ndim1 = 1
+    ndim2 = 1
+    Cn(ndim1, ndim2, tmax, beta)
+end
 
 #=
 *Remarks : Matsubara Green's Function*
