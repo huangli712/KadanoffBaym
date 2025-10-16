68af3b93c0fc90eb238da503198fcfc67fb7daaf
Li Huang <lihuang.dmft@gmail.com>
2025-10-16 11:34:47 +0800

    add operators.jl

--- src/traits.jl
+++ src/traits.jl
@@ -97,53 +97,6 @@ function smul!(mat::Gᵐᵃᵗ{T}, x::Element{T}) where {T}
 end
 
 #=
-### *Gᵐᵃᵗ* : *Traits*
-=#
-
-"""
-    Base.:+(mat1::Gᵐᵃᵗ{T}, mat2::Gᵐᵃᵗ{T})
-
-Operation `+` for two `Gᵐᵃᵗ` objects.
-"""
-function Base.:+(mat1::Gᵐᵃᵗ{T}, mat2::Gᵐᵃᵗ{T}) where {T}
-    # Sanity check
-    @assert getsize(mat1) == getsize(mat2)
-    @assert getdims(mat1) == getdims(mat2)
-
-    Gᵐᵃᵗ(mat1.type, mat1.ntau, mat1.ndim1, mat1.ndim2, mat1.data + mat2.data)
-end
-
-"""
-    Base.:-(mat1::Gᵐᵃᵗ{T}, mat2::Gᵐᵃᵗ{T})
-
-Operation `-` for two `Gᵐᵃᵗ` objects.
-"""
-function Base.:-(mat1::Gᵐᵃᵗ{T}, mat2::Gᵐᵃᵗ{T}) where {T}
-    # Sanity check
-    @assert getsize(mat1) == getsize(mat2)
-    @assert getdims(mat1) == getdims(mat2)
-
-    Gᵐᵃᵗ(mat1.type, mat1.ntau, mat1.ndim1, mat1.ndim2, mat1.data - mat2.data)
-end
-
-"""
-    Base.:*(mat::Gᵐᵃᵗ{T}, x)
-
-Operation `*` for a `Gᵐᵃᵗ` object and a scalar value.
-"""
-function Base.:*(mat::Gᵐᵃᵗ{T}, x) where {T}
-    cx = convert(T, x)
-    Gᵐᵃᵗ(mat.type, mat.ntau, mat.ndim1, mat.ndim2, mat.data * cx)
-end
-
-"""
-    Base.:*(x, mat::Gᵐᵃᵗ{T})
-
-Operation `*` for a scalar value and a `Gᵐᵃᵗ` object.
-"""
-Base.:*(x, mat::Gᵐᵃᵗ{T}) where {T} = Base.:*(mat, x)
-
-#=
 ### *Gʳᵉᵗ* : *Operations*
 =#
 
@@ -267,53 +220,6 @@ function smul!(ret::Gʳᵉᵗ{T}, x::Cf{T}, tstp::I64) where {T}
         ret.data[tstp,i] = ret.data[tstp,i] * x[i]
     end
 end
-
-#=
-### *Gʳᵉᵗ* : *Traits*
-=#
-
-"""
-    Base.:+(ret1::Gʳᵉᵗ{T}, ret2::Gʳᵉᵗ{T})
-
-Operation `+` for two `Gʳᵉᵗ` objects.
-"""
-function Base.:+(ret1::Gʳᵉᵗ{T}, ret2::Gʳᵉᵗ{T}) where {T}
-    # Sanity check
-    @assert getsize(ret1) == getsize(ret2)
-    @assert getdims(ret1) == getdims(ret2)
-
-    Gʳᵉᵗ(ret1.type, ret1.ntime, ret1.ndim1, ret1.ndim2, ret1.data + ret2.data)
-end
-
-"""
-    Base.:-(ret1::Gʳᵉᵗ{T}, ret2::Gʳᵉᵗ{T})
-
-Operation `-` for two `Gʳᵉᵗ` objects.
-"""
-function Base.:-(ret1::Gʳᵉᵗ{T}, ret2::Gʳᵉᵗ{T}) where {T}
-    # Sanity check
-    @assert getsize(ret1) == getsize(ret2)
-    @assert getdims(ret1) == getdims(ret2)
-
-    Gʳᵉᵗ(ret1.type, ret1.ntime, ret1.ndim1, ret1.ndim2, ret1.data - ret2.data)
-end
-
-"""
-    Base.:*(ret::Gʳᵉᵗ{T}, x)
-
-Operation `*` for a `Gʳᵉᵗ` object and a scalar value.
-"""
-function Base.:*(ret::Gʳᵉᵗ{T}, x) where {T}
-    cx = convert(T, x)
-    Gʳᵉᵗ(ret.type, ret.ntime, ret.ndim1, ret.ndim2, ret.data * cx)
-end
-
-"""
-    Base.:*(x, ret::Gʳᵉᵗ{T})
-
-Operation `*` for a scalar value and a `Gʳᵉᵗ` object.
-"""
-Base.:*(x, ret::Gʳᵉᵗ{T}) where {T} = Base.:*(ret, x)
 
 #=
 ### *Gˡᵐⁱˣ* : *Operations*
