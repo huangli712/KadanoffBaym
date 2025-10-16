b1195fb13e0a7d73cfc916bd694b793c8f978980
Li Huang <lihuang.dmft@gmail.com>
2025-10-16 10:07:45 +0800

    remove gfull.jl

--- src/structs.jl
+++ src/structs.jl
@@ -694,3 +694,249 @@ Constructor. All the matrix elements are set to be complex zero.
 function Gˡᵉˢˢ(C::Cn)
     Gˡᵉˢˢ(C.ntime, C.ndim1, C.ndim2, zero(C64))
 end
+
+#=
+### *Gᵐᵃᵗᵐ* : *Struct*
+=#
+
+"""
+    Gᵐᵃᵗᵐ{T}
+
+Matsubara component (``G^M``) of contour Green's function. It is designed
+for ``\tau < 0`` case. It is not an independent component. It can be
+inferred or deduced from the `Gᵐᵃᵗ{T}` struct. We usually call this
+component `matm`.
+
+See also: [`Gʳᵉᵗ`](@ref), [`Gˡᵐⁱˣ`](@ref), [`Gˡᵉˢˢ`](@ref).
+"""
+mutable struct Gᵐᵃᵗᵐ{T} <: CnAbstractMatrix{T}
+    type  :: String
+    sign  :: I64 # Used to distinguish fermions and bosons
+    ntau  :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    dataM :: Ref{Gᵐᵃᵗ{T}}
+end
+
+#=
+### *Gᵐᵃᵗᵐ* : *Constructors*
+=#
+
+"""
+    Gᵐᵃᵗᵐ(sign::I64, mat::Gᵐᵃᵗ{T})
+
+Constructor. Note that the `matm` component is not independent. We use
+the `mat` component to initialize it.
+"""
+function Gᵐᵃᵗᵐ(sign::I64, mat::Gᵐᵃᵗ{T}) where {T}
+    # Sanity check
+    @assert sign in (BOSE, FERMI)
+
+    # Setup properties
+    # Extract parameters from `mat`
+    ntau = mat.ntau
+    ndim1 = mat.ndim1
+    ndim2 = mat.ndim2
+    #
+    # We don't allocate memory for `dataM` directly, but let it point to
+    # the `mat` object.
+    dataM = Ref(mat)
+
+    # Call the default constructor
+    Gᵐᵃᵗᵐ("matm", sign, ntau, ndim1, ndim2, dataM)
+end
+
+#=
+*Remarks : Advanced Green's Function*
+
+The advanced component of contour Green's function reads
+
+```math
+\begin{equation}
+G^{A}(t,t') =
+    i \theta(t'-t) \langle [c(t), c^{\dagger}(t')]_{\mp} \rangle,
+\end{equation}
+```
+
+Here, ``t``, ``t'`` belong to ``\mathcal{C}_1 ∪ \mathcal{C}_2``,
+``\theta(t)`` is a step function, ``[,]_{-(+)}`` denotes an
+(anti-)commutator. We choose the -(+) sign if the operators ``c``
+and ``c^{\dagger}`` are bosonic (fermionic).
+=#
+
+#=
+### *Gᵃᵈᵛ* : *Struct*
+=#
+
+"""
+    Gᵃᵈᵛ{T}
+
+Advanced component (``G^{A}``) of contour Green's function. We usually
+call this component `adv`.
+
+Note that currently we do not need this component explicitly. However,
+for the sake of completeness, we still define an empty struct for it.
+
+See also: [`Gᵐᵃᵗ`](@ref), [`Gˡᵐⁱˣ`](@ref), [`Gˡᵉˢˢ`](@ref).
+"""
+mutable struct Gᵃᵈᵛ{T} <: CnAbstractMatrix{T}
+    type  :: String
+end
+
+#=
+### *Gᵃᵈᵛ* : *Constructors*
+=#
+
+"""
+    Gᵃᵈᵛ()
+
+Constructor. Note that the `adv` component is not independent. We use
+the `ret` component to initialize it.
+"""
+function Gᵃᵈᵛ()
+    # Call the default constructor
+    Gᵃᵈᵛ("adv")
+end
+
+#=
+*Remarks : Right-mixing Green's Function*
+
+The right-mixing component of contour Green's function reads
+
+```math
+\begin{equation}
+G^{\lceil}(\tau,t') =  -i \langle c(\tau) c^{\dagger}(t')  \rangle,
+\end{equation}
+```
+
+where ``t' \in \mathcal{C}_1 \cup \mathcal{C}_2`` and
+``\tau \in \mathcal{C}_3``.
+=#
+
+#=
+### *Gʳᵐⁱˣ* : *Struct*
+=#
+
+"""
+    Gʳᵐⁱˣ{T}
+
+Right-mixing component (``G^{⌈}``) of contour Green's function. We usually
+call this component `rmix`.
+
+See also: [`Gᵐᵃᵗ`](@ref), [`Gʳᵉᵗ`](@ref), [`Gˡᵉˢˢ`](@ref).
+"""
+mutable struct Gʳᵐⁱˣ{T} <: CnAbstractMatrix{T}
+    type  :: String
+    sign  :: I64 # Used to distinguish fermions and bosons
+    ntime :: I64
+    ntau  :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    dataL :: Ref{Gˡᵐⁱˣ{T}}
+end
+
+#=
+### *Gʳᵐⁱˣ* : *Constructors*
+=#
+
+"""
+    Gʳᵐⁱˣ(sign::I64, lmix::Gˡᵐⁱˣ{T})
+
+Constructor. Note that the `rmix` component is not independent. We use
+the `lmix` component to initialize it.
+"""
+function Gʳᵐⁱˣ(sign::I64, lmix::Gˡᵐⁱˣ{T}) where {T}
+    # Sanity check
+    @assert sign in (BOSE, FERMI)
+
+    # Setup properties
+    # Extract parameters from `lmix`
+    ntime = lmix.ntime
+    ntau  = lmix.ntau
+    ndim1 = lmix.ndim1
+    ndim2 = lmix.ndim2
+    #
+    # We don't allocate memory for `dataL` directly, but let it point to
+    # the `lmix` object.
+    dataL = Ref(lmix)
+
+    # Call the default constructor
+    Gʳᵐⁱˣ("rmix", sign, ntime, ntau, ndim1, ndim2, dataL)
+end
+
+#=
+*Remarks : Greater Green's Function*
+
+The greater component of contour Green's function reads
+
+```math
+\begin{equation}
+G^{>}(t,t') = - i \langle c(t) c^{\dagger}(t') \rangle,
+\end{equation}
+```
+
+where ``t,\ t' \in \mathcal{C}_1 \cup \mathcal{C}_2``. Its hermitian
+conjugate yields
+
+```math
+\begin{equation}
+G^{>}(t,t')^{*} = -G^{>}(t',t).
+\end{equation}
+```
+
+The greater component is related to the retarded, advanced, and Keldysh
+Green's functions via
+
+```math
+\begin{equation}
+G^{>} = \frac{1}{2}(G^{K} + G^{R} - G^{A}).
+\end{equation}
+```
+=#
+
+#=
+### *Gᵍᵗʳ* : *Struct*
+=#
+
+"""
+    Gᵍᵗʳ{T}
+
+Greater component (``G^{>}``) of contour Green's function. We usually
+call this component `gtr`.
+
+See also: [`Gʳᵉᵗ`](@ref), [`Gˡᵐⁱˣ`](@ref), [`Gˡᵉˢˢ`](@ref).
+"""
+mutable struct Gᵍᵗʳ{T} <: CnAbstractMatrix{T}
+    type  :: String
+    ntime :: I64
+    ndim1 :: I64
+    ndim2 :: I64
+    dataL :: Ref{Gˡᵉˢˢ{T}}
+    dataR :: Ref{Gʳᵉᵗ{T}}
+end
+
+#=
+### *Gᵍᵗʳ* : *Constructors*
+=#
+
+"""
+    Gᵍᵗʳ(less::Gˡᵉˢˢ{T}, ret::Gʳᵉᵗ{T})
+
+Constructor. Note that the `gtr` component is not independent. We use
+the `less` and `ret` components to initialize it.
+"""
+function Gᵍᵗʳ(less::Gˡᵉˢˢ{T}, ret::Gʳᵉᵗ{T}) where {T}
+    # Setup properties
+    # Extract parameters from `less`
+    ntime = less.ntime
+    ndim1 = less.ndim1
+    ndim2 = less.ndim2
+    #
+    # We don't allocate memory for `dataL` and `dataR` directly, but
+    # let them point to  `less` and `ret` objects, respectively.
+    dataL = Ref(less)
+    dataR = Ref(ret)
+
+    # Call the default constructor
+    Gᵍᵗʳ("gtr", ntime, ndim1, ndim2, dataL, dataR)
+end
--- src/traits.jl
+++ src/traits.jl
@@ -1208,3 +1208,70 @@ end
 Operation `*` for a scalar value and a `Gˡᵉˢˢ` object.
 """
 Base.:*(x, less::Gˡᵉˢˢ{T}) where {T} = Base.:*(less, x)
+
+#=
+### *Gᵐᵃᵗᵐ* : *Indexing*
+=#
+
+"""
+    Base.getindex(matm::Gᵐᵃᵗᵐ{T}, ind::I64)
+
+Visit the element stored in `Gᵐᵃᵗᵐ` object.
+"""
+function Base.getindex(matm::Gᵐᵃᵗᵐ{T}, ind::I64) where {T}
+    # Sanity check
+    @assert 1 ≤ ind ≤ matm.ntau
+
+    # Return G^{M}(τᵢ < 0)
+    matm.dataM[][matm.ntau - ind + 1] * matm.sign
+end
+
+#=
+### *Gᵃᵈᵛ* : *Indexing*
+=#
+
+"""
+    Base.getindex(adv::Gᵃᵈᵛ{T}, ind::I64)
+
+Visit the element stored in `Gᵃᵈᵛ` object.
+"""
+function Base.getindex(adv::Gᵃᵈᵛ{T}, ind::I64) where {T}
+    sorry()
+end
+
+#=
+### *Gʳᵐⁱˣ* : *Indexing*
+=#
+
+"""
+    Base.getindex(rmix::Gʳᵐⁱˣ{T}, i::I64, j::I64)
+
+Visit the element stored in `Gʳᵐⁱˣ` object.
+"""
+function Base.getindex(rmix::Gʳᵐⁱˣ{T}, i::I64, j::I64) where {T}
+    # Sanity check
+    @assert 1 ≤ i ≤ rmix.ntau
+    @assert 1 ≤ j ≤ rmix.ntime
+
+    # Return G^{⌈}(τᵢ, tⱼ)
+    (rmix.dataL[])[j,rmix.ntau - i + 1]' * (-rmix.sign)
+end
+
+#=
+### *Gᵍᵗʳ* : *Indexing*
+=#
+
+"""
+    Base.getindex(gtr::Gᵍᵗʳ{T}, i::I64, j::I64)
+
+Visit the element stored in `Gᵍᵗʳ` object.
+"""
+function Base.getindex(gtr::Gᵍᵗʳ{T}, i::I64, j::I64) where {T}
+    # Sanity check
+    @assert 1 ≤ i ≤ gtr.ntime
+    @assert 1 ≤ j ≤ gtr.ntime
+
+    # Return G^{>}(tᵢ, tⱼ)
+    gtr.dataL[][i,j] + gtr.dataR[][i,j]
+end
+
