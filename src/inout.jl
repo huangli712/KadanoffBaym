8487b37071908e11d845269ec53a6dd959b810d2
Li Huang <lihuang.dmft@gmail.com>
2025-10-25 00:47:42 +0800

    fix inout.jl

--- src/inout.jl
+++ src/inout.jl
@@ -45,7 +45,7 @@ function Base.show(io::IO, mat::Gᵐᵃᵗ{T}) where {T}
     println(io, "ntau: ", mat.ntau)
     println(io, "ndim1:", mat.ndim1)
     println(io, "ndim2:", mat.ndim2)
-    println(io, "data:", data)
+    println(io, "data:", mat.data)
 end
 
 #=
