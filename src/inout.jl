55417d20ee2195ba5f01e0a762b1345aeb620f39
Li Huang <lihuang.dmft@gmail.com>
2025-10-30 10:07:35 +0800

    implement Base.read!() for mat component

--- src/inout.jl
+++ src/inout.jl
@@ -236,7 +236,47 @@ See also: [`Gᵐᵃᵗ`](@ref).
 """
 function Base.read!(fname::AbstractString, mat::Gᵐᵃᵗ{T}) where {T}
     if isfile(fname)
-        # TODO
+        open(fname, "r") do fin
+            readline(fin) # Skip the comment line
+            #
+            # Extract parameters
+            arr = line_to_array(fin)
+            mat.type = arr[3]
+            arr = line_to_array(fin)
+            mat.ntau = parse(I64, arr[3])
+            arr = line_to_array(fin)
+            mat.ndim1 = parse(I64, arr[3])
+            arr = line_to_array(fin)
+            mat.ndim2 = parse(I64, arr[3])
+            #
+            readline(fin) # Skip the comment line
+            #
+            # Prepare memory
+            element = fill(zero(T), mat.ndim1, mat.ndim2)
+            mat.data = MatArray{T}(undef, mat.ntau, 1)
+            #
+            # Extract function data
+            for i = 1:getsize(mat)
+                readline(fin) # Skip the comment line
+                #
+                for m = 1:cf.ndim1
+                    for n = 1:cf.ndim2
+                        if T == F64
+                            arr = line_to_array(fin)
+                            element[n,m] = parse(F64, arr[3])
+                        elseif T == C64
+                            arr = line_to_array(fin)
+                            element[n,m] = parse(F64, arr[3]) + parse(F64, arr[4]) * im
+                        else
+                            error("The datatype $T is unsupported!")
+                        end
+                    end
+                end
+                #
+                mat.data[i,1] = copy(element)
+            end
+        end
+        @show mat
     else
         error("The $fname file doesn't exist!")
     end
