e6ee88189b2793b2e38a7f48a2694ada9201047e
Li Huang <lihuang.dmft@gmail.com>
2025-10-30 17:43:28 +0800

    fix inout.jl

--- src/inout.jl
+++ src/inout.jl
@@ -248,6 +248,47 @@ function Base.write(fname::AbstractString, mat::Gᵐᵃᵗ{T}) where {T}
     end
 end
 
+function Base.read!(io::IO, mat::Gᵐᵃᵗ{T}) where {T}
+    readline(io) # Skip the comment line
+    #
+    # Extract parameters
+    arr = line_to_array(io)
+    mat.type = arr[3]
+    arr = line_to_array(io)
+    mat.ntau = parse(I64, arr[3])
+    arr = line_to_array(io)
+    mat.ndim1 = parse(I64, arr[3])
+    arr = line_to_array(io)
+    mat.ndim2 = parse(I64, arr[3])
+    #
+    readline(io) # Skip the comment line
+    #
+    # Prepare memory
+    element = fill(zero(T), mat.ndim1, mat.ndim2)
+    mat.data = MatArray{T}(undef, mat.ntau, 1)
+    #
+    # Extract function data
+    for i = 1:getsize(mat)
+        readline(io) # Skip the comment line
+        #
+        for m = 1:mat.ndim2
+            for n = 1:mat.ndim1
+                if T == F64
+                    arr = line_to_array(io)
+                    element[n,m] = parse(F64, arr[3])
+                elseif T == C64
+                    arr = line_to_array(io)
+                    element[n,m] = parse(F64, arr[3]) + parse(F64, arr[4]) * im
+                else
+                    error("The datatype $T is unsupported!")
+                end
+            end
+        end
+        #
+        mat.data[i,1] = copy(element)
+    end
+end
+
 """
     Base.read!(fname::AbstractString, mat::Gᵐᵃᵗ{T})
 
@@ -259,44 +300,7 @@ See also: [`Gᵐᵃᵗ`](@ref).
 function Base.read!(fname::AbstractString, mat::Gᵐᵃᵗ{T}) where {T}
     if isfile(fname)
         open(fname, "r") do fin
-            readline(fin) # Skip the comment line
-            #
-            # Extract parameters
-            arr = line_to_array(fin)
-            mat.type = arr[3]
-            arr = line_to_array(fin)
-            mat.ntau = parse(I64, arr[3])
-            arr = line_to_array(fin)
-            mat.ndim1 = parse(I64, arr[3])
-            arr = line_to_array(fin)
-            mat.ndim2 = parse(I64, arr[3])
-            #
-            readline(fin) # Skip the comment line
-            #
-            # Prepare memory
-            element = fill(zero(T), mat.ndim1, mat.ndim2)
-            mat.data = MatArray{T}(undef, mat.ntau, 1)
-            #
-            # Extract function data
-            for i = 1:getsize(mat)
-                readline(fin) # Skip the comment line
-                #
-                for m = 1:mat.ndim2
-                    for n = 1:mat.ndim1
-                        if T == F64
-                            arr = line_to_array(fin)
-                            element[n,m] = parse(F64, arr[3])
-                        elseif T == C64
-                            arr = line_to_array(fin)
-                            element[n,m] = parse(F64, arr[3]) + parse(F64, arr[4]) * im
-                        else
-                            error("The datatype $T is unsupported!")
-                        end
-                    end
-                end
-                #
-                mat.data[i,1] = copy(element)
-            end
+            Base.read!(fin, mat)
         end
     else
         error("The $fname file doesn't exist!")
