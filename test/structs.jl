a4689a5ab049f341bee856b1b4cf823b7900d3ea
Li Huang <lihuang.dmft@gmail.com>
2025-11-11 00:38:30 +0800

    fix structs.jl

--- test/structs.jl
+++ test/structs.jl
@@ -3,7 +3,15 @@ haskey(ENV,"KADANOFF_BAYM_HOME") && pushfirst!(LOAD_PATH, ENV["KADANOFF_BAYM_HOM
 using Test
 using KadanoffBaym
 
-@testset "Cn Struct" begin
-    @test 1+1 == 2
-    @test 2+2 == 4
+@testset "Cn Struct: Constructor" begin
+    ntime = 201
+    ntau = 1001
+    ndim1 = 2
+    ndim2 = 2
+    tmax = 5.0
+    beta = 4.0
+
+    C₁ = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+    C₂ = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+    @test  C₁ == C₂
 end
