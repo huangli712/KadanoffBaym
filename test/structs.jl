df1dbc2419d1edef3de7cd54de0f9164b83b4c2b
Li Huang <lihuang.dmft@gmail.com>
2025-11-12 10:02:22 +0800

    fix structs.jl

--- test/structs.jl
+++ test/structs.jl
@@ -50,7 +50,7 @@ using KadanoffBaym
         @test cf₁ == cf₇
     end
     #
-    @testset "Gᵐᵃᵗ Strucct: Constructors" begin
+    @testset "Gᵐᵃᵗ  Struct: Constructors" begin
         type = "mat"
         ntime = 201
         ntau = 1001
@@ -77,5 +77,52 @@ using KadanoffBaym
         @test mat₁ == mat₅
         @test mat₁ == mat₆
         @test mat₁ == mat₇
+    end
+    #
+    @testset "Gʳᵉᵗ  Struct: Constructors" begin
+        type = "ret"
+        ntime = 201
+        ntau = 1001
+        ndim1 = 2
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        v = zero(C64)
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        x = zeros(C64, ndim1, ndim2)
+        #
+        ret₁ = Gʳᵉᵗ(ntime, ndim1, ndim2, v)
+        ret₂ = Gʳᵉᵗ(ntime, ndim1, ndim2)
+        ret₃ = Gʳᵉᵗ(ntime, ndim1)
+        ret₄ = Gʳᵉᵗ(ntime, x)
+        ret₅ = Gʳᵉᵗ(C, x)
+        ret₆ = Gʳᵉᵗ(C, v)
+        ret₇ = Gʳᵉᵗ(C)
+        #
+        @test ret₁ == ret₂
+        @test ret₁ == ret₃
+        @test ret₁ == ret₄
+        @test ret₁ == ret₅
+        @test ret₁ == ret₆
+        @test ret₁ == ret₇
+    end
+    #
+    @testset "Gˡᵐⁱˣ Struct: Constructors" begin
+    end
+    #
+    @testset "Gˡᵉˢˢ Struct: Constructors" begin
+    end
+    #
+    @testset "gᵐᵃᵗ  Struct: Constructors" begin
+    end
+    #
+    @testset "gʳᵉᵗ  Struct: Constructors" begin
+    end
+    #
+    @testset "gˡᵐⁱˣ Struct: Constructors" begin
+    end
+    #
+    @testset "gˡᵉˢˢ Struct: Constructors" begin
     end
 end
