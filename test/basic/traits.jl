25e2a70f053cdc8abd0f9bb89127b7284969605a
Li Huang <lihuang.dmft@gmail.com>
2025-11-13 00:11:25 +0800

    fix traits.jl

---
+++ test/basic/traits.jl
@@ -0,0 +1,7 @@
+haskey(ENV,"KADANOFF_BAYM_HOME") && pushfirst!(LOAD_PATH, ENV["KADANOFF_BAYM_HOME"])
+
+using Test
+using KadanoffBaym
+
+@testset verbose = true "KadanoffBaym: weights.jl" begin
+end
