66b0e444eac243279da9aecfaf6faef730aa292f
Li Huang <lihuang.dmft@gmail.com>
2025-11-18 01:13:52 +0800

    fix inout.jl

--- test/basic/inout.jl
+++ test/basic/inout.jl
@@ -17,15 +17,17 @@
         C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
         C₁ = Cn(2.0, 2.0)
         C₂ = Cn(3.0, 3.0)
-        write(fn, C)
         #
         @test C != C₁
         @test C != C₂
+        #
+        write(fn, C)
         read!(fn, C₁)
-        @test C == C₁
         open(fn, "r") do fin
             read!(fn, C₂)
         end
+        #
+        @test C == C₁
         @test C == C₂
     end
 end
