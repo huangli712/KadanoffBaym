a327043deedf014a78693483fb628bcbf1d3a928
Li Huang <lihuang.dmft@gmail.com>
2025-11-18 01:25:47 +0800

    fix inout.jl

--- test/basic/inout.jl
+++ test/basic/inout.jl
@@ -24,10 +24,71 @@
         write(fn, C)
         read!(fn, C₁)
         open(fn, "r") do fin
-            read!(fn, C₂)
+            read!(fin, C₂)
         end
         #
         @test C == C₁
         @test C == C₂
+    end
+    #
+    @testset "Cf    Struct: read/write (cmplx)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 1
+        ndim2 = 1
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0 - 0.3im
+        v₁ = 0.3 + 0.3im
+        v₂ = 1.2 - 0.3im
+        fn = "Cf.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        cf = Cf(C, v)
+        cf₁ = Cf(C, v₁)
+        cf₂ = Cf(C, v₂)
+        #
+        @test cf != cf₁
+        @test cf != cf₂
+        #
+        write(fn, cf)
+        read!(fn, cf₁)
+        open(fn, "r") do fin
+            read!(fin, cf₂)
+        end
+        #
+        @test cf == cf₁
+        @test cf == cf₂
+    end
+    #
+    #
+    @testset "Cf    Struct: read/write (real)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 1
+        ndim2 = 1
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0
+        v₁ = 0.3
+        v₂ = 1.2
+        fn = "Cf.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        cf = Cf(C, v)
+        cf₁ = Cf(C, v₁)
+        cf₂ = Cf(C, v₂)
+        #
+        @test cf != cf₁
+        @test cf != cf₂
+        #
+        write(fn, cf)
+        read!(fn, cf₁)
+        open(fn, "r") do fin
+            read!(fin, cf₂)
+        end
+        #
+        @test cf == cf₁
+        @test cf == cf₂
     end
 end
