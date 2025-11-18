91f02f789c779a474e6f0e64413aabe89188eeeb
Li Huang <lihuang.dmft@gmail.com>
2025-11-18 15:23:06 +0800

    fix inout.jl

--- test/basic/inout.jl
+++ test/basic/inout.jl
@@ -540,4 +540,34 @@
         @test less₁ == less₂
         @test less₁ == less₃
     end
+    #
+    @testset "gˡᵉˢˢ Struct: read/write (real)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 2
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        tstp = ntime
+        v₁ = 1.0
+        v₂ = 0.3
+        v₃ = 1.2
+        fn = "less.data"
+        #
+        less₁ = gˡᵉˢˢ(tstp, ndim1, ndim2, v₁)
+        less₂ = gˡᵉˢˢ(tstp, ndim1, ndim2, v₂)
+        less₃ = gˡᵉˢˢ(tstp, ndim1, ndim2, v₃)
+        #
+        @test less₁ != less₂
+        @test less₁ != less₃
+        #
+        write(fn, less₁)
+        read!(fn, less₂)
+        open(fn, "r") do fin
+            read!(fin, less₃)
+        end
+        #
+        @test less₁ == less₂
+        @test less₁ == less₃
+    end
 end
