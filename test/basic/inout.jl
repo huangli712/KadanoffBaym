4e3c0edbdb6069308ac5cb9d2a48fc80a32f515c
Li Huang <lihuang.dmft@gmail.com>
2025-11-18 10:58:02 +0800

    fix inout.jl

--- test/basic/inout.jl
+++ test/basic/inout.jl
@@ -212,9 +212,123 @@
     end
     #
     @testset "Gˡᵐⁱˣ Struct: read/write (complx)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 3
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0 - 0.3im
+        v₁ = 0.3 + 0.3im
+        v₂ = 1.2 - 0.3im
+        fn = "lmix.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        lmix = Gˡᵐⁱˣ(C, v)
+        lmix₁ = Gˡᵐⁱˣ(C, v₁)
+        lmix₂ = Gˡᵐⁱˣ(C, v₂)
+        #
+        @test lmix != lmix₁
+        @test lmix != lmix₂
+        #
+        write(fn, lmix)
+        read!(fn, lmix₁)
+        open(fn, "r") do fin
+            read!(fin, lmix₂)
+        end
+        #
+        @test lmix == lmix₁
+        @test lmix == lmix₂
+    end
+    #
+    @testset "Gˡᵐⁱˣ Struct: read/write (real)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 3
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0
+        v₁ = 0.3
+        v₂ = 1.2
+        fn = "lmix.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        lmix = Gˡᵐⁱˣ(C, v)
+        lmix₁ = Gˡᵐⁱˣ(C, v₁)
+        lmix₂ = Gˡᵐⁱˣ(C, v₂)
+        #
+        @test lmix != lmix₁
+        @test lmix != lmix₂
+        #
+        write(fn, lmix)
+        read!(fn, lmix₁)
+        open(fn, "r") do fin
+            read!(fin, lmix₂)
+        end
+        #
+        @test lmix == lmix₁
+        @test lmix == lmix₂
     end
     #
     @testset "Gˡᵉˢˢ Struct: read/write (complx)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 3
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0 - 0.3im
+        v₁ = 0.3 + 0.3im
+        v₂ = 1.2 - 0.3im
+        fn = "less.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        less = Gˡᵉˢˢ(C, v)
+        less₁ = Gˡᵉˢˢ(C, v₁)
+        less₂ = Gˡᵉˢˢ(C, v₂)
+        #
+        @test less != less₁
+        @test less != less₂
+        #
+        write(fn, less)
+        read!(fn, less₁)
+        open(fn, "r") do fin
+            read!(fin, less₂)
+        end
+        #
+        @test less == less₁
+        @test less == less₂
+    end
+    #
+    @testset "Gˡᵉˢˢ Struct: read/write (real)" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 3
+        ndim2 = 2
+        tmax = 5.0
+        beta = 4.0
+        v = 1.0
+        v₁ = 0.3
+        v₂ = 1.2
+        fn = "less.data"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        less = Gˡᵉˢˢ(C, v)
+        less₁ = Gˡᵉˢˢ(C, v₁)
+        less₂ = Gˡᵉˢˢ(C, v₂)
+        #
+        @test less != less₁
+        @test less != less₂
+        #
+        write(fn, less)
+        read!(fn, less₁)
+        open(fn, "r") do fin
+            read!(fin, less₂)
+        end
+        #
+        @test less == less₁
+        @test less == less₂
     end
     #
     @testset "gᵐᵃᵗ  Struct: read/write (complx)" begin
