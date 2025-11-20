20727c0d82ca0e869bcc46f7a4a338e9e0fe901c
Li Huang <lihuang.dmft@gmail.com>
2025-11-21 01:15:29 +0800

    fix config.jl

--- test/basic/config.jl
+++ test/basic/config.jl
@@ -5,11 +5,12 @@
 #
 
 @testset verbose = true "KadanoffBaym: config.jl" begin
-    @testset "Cf    Struct: getindex/setindex" begin
+    @testset "Configuration file parser" begin
         push!(ARGS, "neq.toml")
         cfg = inp_toml(query_args(), true)
         fil_dict(cfg)
         chk_dict()
+        see_dict()
         #
         @test get_c("ntime") == 101
         @test get_c("ntau") == 21
@@ -18,6 +19,30 @@
         @test get_c("tmax") == 3.0
         @test get_c("beta") == 4.0
         #
+        @test get_m("system") == "Hubbard"
+        #
+        rev_dict_c(_PCONTOUR)
+        @test get_c("ntime") == 1001
+        @test get_c("ntau") == 201
+        @test get_c("ndim1") == 1
+        @test get_c("ndim2") == 1
+        @test get_c("tmax") == 4.0
+        @test get_c("beta") == 8.0
+        #
+        rev_dict_m(_PMODEL)
+        @test get_m("system") == "unknown"
+        #
+        contour = inp_toml(query_args(), "contour", true)
+        rev_dict_c(contour)
+        @test get_c("ntime") == 101
+        @test get_c("ntau") == 21
+        @test get_c("ndim1") == 2
+        @test get_c("ndim2") == 3
+        @test get_c("tmax") == 3.0
+        @test get_c("beta") == 4.0
+        #
+        model = inp_toml(query_args(), "model", true)
+        rev_dict_m(model)
         @test get_m("system") == "Hubbard"
     end
 end
