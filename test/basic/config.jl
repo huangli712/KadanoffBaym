6e992a4cb93053f1e86bbf0ef51d3a31e29f6016
Li Huang <lihuang.dmft@gmail.com>
2025-11-21 01:02:12 +0800

    fix config.jl

--- test/basic/config.jl
+++ test/basic/config.jl
@@ -4,3 +4,20 @@
 # To test whether the configuration parser works correctly.
 #
 
+@testset verbose = true "KadanoffBaym: config.jl" begin
+    @testset "Cf    Struct: getindex/setindex" begin
+        push!(ARGS, "neq.toml")
+        cfg = inp_toml(query_args(), true)
+        fil_dict(cfg)
+        chk_dict()
+        #
+        @test get_c("ntime") == 101
+        @test get_c("ntau") == 21
+        @test get_c("ndim1") == 2
+        @test get_c("ndim2") == 3
+        @test get_c("tmax") == 3.0
+        @test get_c("beta") == 4.0
+        #
+        @test get_m("system") == "Hubbard"
+    end
+end
--- test/basic/runtests.jl
+++ test/basic/runtests.jl
@@ -3,6 +3,7 @@ haskey(ENV,"KADANOFF_BAYM_HOME") && pushfirst!(LOAD_PATH, ENV["KADANOFF_BAYM_HOM
 using Test
 using KadanoffBaym
 
+include("config.jl")
 
 #include("structs.jl")
 #include("properties.jl")
