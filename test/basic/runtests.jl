62cbda1c67e4792f7d020db4551d55a70da0983a
Li Huang <lihuang.dmft@gmail.com>
2025-11-22 01:02:17 +0800

    rename T_structs.jl

--- test/basic/runtests.jl
+++ test/basic/runtests.jl
@@ -3,11 +3,11 @@ haskey(ENV,"KADANOFF_BAYM_HOME") && pushfirst!(LOAD_PATH, ENV["KADANOFF_BAYM_HOM
 using Test
 using KadanoffBaym
 
-include("config.jl")
-
-#include("structs.jl")
+include("T_structs.jl")
 #include("properties.jl")
 #include("indexing.jl")
 #include("inout.jl")
 #include("weights.jl")
 #include("traits.jl")
+
+#include("config.jl")
