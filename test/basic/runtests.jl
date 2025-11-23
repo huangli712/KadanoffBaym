97be128820938a69e1aeb1a20ab01611925d369a
Li Huang <lihuang.dmft@gmail.com>
2025-11-24 01:32:23 +0800

    fix runtests.jl

--- test/basic/runtests.jl
+++ test/basic/runtests.jl
@@ -6,8 +6,8 @@ using KadanoffBaym
 #include("T_structs.jl")
 #include("properties.jl")
 #include("indexing.jl")
-include("T_inout.jl")
+#include("T_inout.jl")
 #include("weights.jl")
 #include("traits.jl")
 
-#include("config.jl")
+include("S_config.jl")
