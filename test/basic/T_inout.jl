48c61b16d0d1088b32b8c5b66948c82036eb3828
Li Huang <lihuang.dmft@gmail.com>
2025-11-23 17:34:19 +0800

    fix T_inout.jl

--- test/basic/T_inout.jl
+++ test/basic/T_inout.jl
@@ -46,12 +46,15 @@
         ndim2 = 1
         tmax = 5.0
         beta = 4.0
+        #
+        fn = "Cf.data"
+        wfn = "Cf.wrong"
+        #
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
         v₁ = 1.0 - 0.3im
         v₂ = 0.3 + 0.3im
         v₃ = 1.2 - 0.3im
-        fn = "Cf.data"
         #
-        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
         cf₁ = Cf(C, v₁)
         cf₂ = Cf(C, v₂)
         cf₃ = Cf(C, v₃)
@@ -67,6 +70,12 @@
         #
         @test cf₁ == cf₂
         @test cf₁ == cf₃
+        #
+        try
+            read!(wfn, cf₂)
+        catch ex
+            catch_error()
+        end
     end
     #
     @testset "Cf    Struct: read/write (real)" begin
