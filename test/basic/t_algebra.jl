e2b2858a91a515b61da9885674967a40e416c521
Li Huang <lihuang.dmft@gmail.com>
2025-12-01 23:53:18 +0800

    fix t_algebra.jl

--- test/basic/t_algebra.jl
+++ test/basic/t_algebra.jl
@@ -28,7 +28,35 @@
         @test cf₃ == cf₁ * 3.0
         @test cf₂ == 2.0 * cf₁
         @test cf₂ == cf₁ * 2.0
+        @test cf₃ == 2.0 * cf₂ - 1.0 * cf₁
     end
+    #
+    @testset "Gᵐᵃᵗ  Struct: basic algebra" begin
+        ntau = 1001
+        ndim1 = 2
+        ndim2 = 2
+        #
+        v₁ = 1.0 + 0.0im
+        v₂ = 2.0 + 0.0im
+        v₃ = 3.0 + 0.0im
+        x₁ = fill(v₁, (ndim1, ndim2))
+        x₂ = fill(v₂, (ndim1, ndim2))
+        x₃ = fill(v₃, (ndim1, ndim2))
+        #
+        mat₁ = Gᵐᵃᵗ(ntau, ndim1, ndim2, v₁)
+        mat₂ = Gᵐᵃᵗ(ntau, ndim1, ndim2, v₂)
+        mat₃ = Gᵐᵃᵗ(ntau, ndim1, ndim2, v₃)
+        #
+        @test mat₃ == mat₁ + mat₂
+        @test mat₂ == mat₃ - mat₁
+        @test mat₁ == mat₃ - mat₂
+        @test mat₃ == 3.0 * mat₁
+        @test mat₃ == mat₁ * 3.0
+        @test mat₂ == 2.0 * mat₁
+        @test mat₂ == mat₁ * 2.0
+        @test mat₃ == 2.0 * mat₂ - 1.0 * mat₁
+    end
+    #
 end
 
 println("All tests pass!\n")
