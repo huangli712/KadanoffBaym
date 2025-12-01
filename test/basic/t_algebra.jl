73dacde0d5d9f08271e32f1ff3b668ef20e853c0
Li Huang <lihuang.dmft@gmail.com>
2025-12-02 00:05:40 +0800

    fix t_algebra.jl

--- test/basic/t_algebra.jl
+++ test/basic/t_algebra.jl
@@ -82,6 +82,59 @@
         @test ret₂ == ret₁ * 2.0
         @test ret₃ == 2.0 * ret₂ - 1.0 * ret₁
     end
+    #
+    @testset "Gˡᵐⁱˣ Struct: basic algebra" begin
+        ntime = 201
+        ntau = 1001
+        ndim1 = 2
+        ndim2 = 2
+        #
+        v₁ = 1.0 + 0.0im
+        v₂ = 2.0 + 0.0im
+        v₃ = 3.0 + 0.0im
+        x₁ = fill(v₁, (ndim1, ndim2))
+        x₂ = fill(v₂, (ndim1, ndim2))
+        x₃ = fill(v₃, (ndim1, ndim2))
+        #
+        lmix₁ = Gˡᵐⁱˣ(ntime, ntau, ndim1, ndim2, v₁)
+        lmix₂ = Gˡᵐⁱˣ(ntime, ntau, ndim1, ndim2, v₂)
+        lmix₃ = Gˡᵐⁱˣ(ntime, ntau, ndim1, ndim2, v₃)
+        #
+        @test lmix₃ == lmix₁ + lmix₂
+        @test lmix₂ == lmix₃ - lmix₁
+        @test lmix₁ == lmix₃ - lmix₂
+        @test lmix₃ == 3.0 * lmix₁
+        @test lmix₃ == lmix₁ * 3.0
+        @test lmix₂ == 2.0 * lmix₁
+        @test lmix₂ == lmix₁ * 2.0
+        @test lmix₃ == 2.0 * lmix₂ - 1.0 * lmix₁
+    end
+    #
+    @testset "Gˡᵉˢˢ Struct: basic algebra" begin
+        ntime = 201
+        ndim1 = 2
+        ndim2 = 2
+        #
+        v₁ = 1.0 + 0.0im
+        v₂ = 2.0 + 0.0im
+        v₃ = 3.0 + 0.0im
+        x₁ = fill(v₁, (ndim1, ndim2))
+        x₂ = fill(v₂, (ndim1, ndim2))
+        x₃ = fill(v₃, (ndim1, ndim2))
+        #
+        less₁ = Gˡᵉˢˢ(ntime, ndim1, ndim2, v₁)
+        less₂ = Gˡᵉˢˢ(ntime, ndim1, ndim2, v₂)
+        less₃ = Gˡᵉˢˢ(ntime, ndim1, ndim2, v₃)
+        #
+        @test less₃ == less₁ + less₂
+        @test less₂ == less₃ - less₁
+        @test less₁ == less₃ - less₂
+        @test less₃ == 3.0 * less₁
+        @test less₃ == less₁ * 3.0
+        @test less₂ == 2.0 * less₁
+        @test less₂ == less₁ * 2.0
+        @test less₃ == 2.0 * less₂ - 1.0 * less₁
+    end
 end
 
 println("All tests pass!\n")
