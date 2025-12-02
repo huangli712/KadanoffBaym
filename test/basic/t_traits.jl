61a0fbaa1a76fdc823cbc10f8a790e31980a5256
Li Huang <lihuang.dmft@gmail.com>
2025-12-02 12:04:18 +0800

    fix t_traits.jl

--- test/basic/t_traits.jl
+++ test/basic/t_traits.jl
@@ -178,51 +178,54 @@
         @test err < ϵ
     end
     #
-    @testset "smul! (complex weight)" begin
-        mat1 = fill(zero(C64), ndim1, ndim1)
-        mat3 = fill(zero(C64), ndim1, ndim1)
+    @testset "comprehensive test 4" begin
+        mat₁ = fill(zero(C64), ndim1, ndim1)
+        mat₃ = fill(zero(C64), ndim1, ndim1)
         #
-        ret1 = fill(zero(C64), ndim1, ndim1)
-        ret3 = fill(zero(C64), ndim1, ndim1)
+        ret₁ = fill(zero(C64), ndim1, ndim1)
+        ret₃ = fill(zero(C64), ndim1, ndim1)
         #
-        lmix1 = fill(zero(C64), ndim1, ndim1)
-        lmix3 = fill(zero(C64), ndim1, ndim1)
+        lmix₁ = fill(zero(C64), ndim1, ndim1)
+        lmix₃ = fill(zero(C64), ndim1, ndim1)
         #
-        less1 = fill(zero(C64), ndim1, ndim1)
-        less3 = fill(zero(C64), ndim1, ndim1)
+        less₁ = fill(zero(C64), ndim1, ndim1)
+        less₃ = fill(zero(C64), ndim1, ndim1)
+
+        # G₃ = G₁ * wz
 
         # For mat component
         for q=1:ntau
-            @. mat1 = G1.mat[q]
-            @. mat3 = mat1 * wz
-            G3.mat[q] = mat3
+            @. mat₁ = G₁.mat[q]
+            @. mat₃ = mat₁ * wz
+            G₃.mat[q] = mat₃
         end
 
         for i=1:ntime
             # For ret and less components
             for j=1:i
-                @. ret1 = G1.ret[i,j]
-                @. ret3 = ret1 * wz
-                G3.ret[i,j] = ret3
+                @. ret₁ = G₁.ret[i,j]
+                @. ret₃ = ret₁ * wz
+                G₃.ret[i,j] = ret₃
 
-                @. less1 = G1.less[j,i]
-                @. less3 = less1 * wz
-                G3.less[j,i] = less3
+                @. less₁ = G₁.less[j,i]
+                @. less₃ = less₁ * wz
+                G₃.less[j,i] = less₃
             end
 
             # For lmix component
             for q=1:ntau
-                @. lmix1 = G1.lmix[i,q]
-                @. lmix3 = lmix1 * wz
-                G3.lmix[i,q] = lmix3
+                @. lmix₁ = G₁.lmix[i,q]
+                @. lmix₃ = lmix₁ * wz
+                G₃.lmix[i,q] = lmix₃
             end
         end
 
+        # Actually, G₄ = G₁ * wz = G₃
         err = 0.0
-        init_green!(G4, H1, mu, beta, dt)
+        init_green!(G₄, H₁, μ, beta, δt)
         for tstp = 0:ntime
-            smul!(G4, tstp, wz)
-            err = err + distance(G3, G4, tstp)
+            smul!(G₄, tstp, wz)
+            err = err + distance(G₃, G₄, tstp)
         end
         @test err < ϵ
     end
