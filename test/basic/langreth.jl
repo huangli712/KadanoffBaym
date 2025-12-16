2a2db70807dac8675073ca23220d2348e86eff49
Li Huang <lihuang.dmft@gmail.com>
2025-12-16 11:53:30 +0800

    fix langreth.jl

--- test/basic/langreth.jl
+++ test/basic/langreth.jl
@@ -4,7 +4,7 @@ using KadanoffBaym
 
 # Parameters
 ntime = 8
-ntau = 501
+ntau = 21
 beta = 5.0
 h = 0.01
 wa = 1.123
@@ -16,14 +16,14 @@ eps = 1.0e-6
 ndim1 = 1; ndim2 = 1
 
 # Contour and Green's functions
-C = Cn(ntime, ntau, ndim1, ndim1, tmax, beta)
+C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
 A = ℱ(C, BOSE)
 B = ℱ(C, BOSE)
 AB = ℱ(C, BOSE)
 
 # H₀
-eps_a = fill(zero(C64), ndim1, ndim1)
-eps_b = fill(zero(C64), ndim1, ndim1)
+eps_a = fill(zero(C64), ndim1, ndim2)
+eps_b = fill(zero(C64), ndim1, ndim2)
 eps_a[1,1] = wa
 eps_b[1,1] = wb
 
@@ -40,6 +40,6 @@ I = Integrator(k)
 #c_mat_mat_2(m, AB.mat, A.mat, B.mat, I, A.sign)
 
 for m = 1:ntau
-    c_mat_mat_1(m, AB.mat, A.mat, B.mat, I, A.sign)
+    c_mat_mat_2(m, AB.mat, A.mat, B.mat, I, A.sign)
     @show m, AB.mat[m]
 end
