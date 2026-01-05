9961adabcea4fd255ced4bb1479ea0f282b88e29
Li Huang <lihuang.dmft@gmail.com>
2026-01-05 10:30:27 +0800

    fix version number

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This toolkit is currently under heavy developement. **IT DOES NOT WORK NOW!**
 
 ## Version
 
-v0.1.9-devel.251119
+v0.2.0-devel.260105
 
 ## License
 
--- src/global.jl
+++ src/global.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2025/11/19
+# Last modified: 2026/01/05
 #
 
 #=
@@ -172,7 +172,7 @@ Version of this julia toolkit.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.1.9-devel.251119"
+const __VERSION__ = v"0.2.0-devel.260105"
 
 """
     __RELEASE__
@@ -181,7 +181,7 @@ Release date of this julia toolkit.
 
 See also: [`__AUTHORS__`](@ref).
 """
-const __RELEASE__ = "2025/11"
+const __RELEASE__ = "2026/01"
 
 #=
 *Remarks* :
