286e3415116f4b36f65346fd160405b78487c87b
Li Huang <lihuang.dmft@gmail.com>
2026-01-06 16:45:55 +0800

    fix convolution.jl

--- test/basic/convolution.jl
+++ test/basic/convolution.jl
@@ -70,9 +70,18 @@ I = Integrator(k)
 #    println()
 #end
 
+#for n = 1:ntime
+#    conv_ret_lmix(n, AB.lmix, A.ret, A.ret, B.lmix, B.lmix, I, h)
+#    conv_lmix_mat(n, AB.lmix, A.lmix, B.mat, I, beta, A.sign)
+#    for m = 1:ntau
+#        @show n, m, AB.lmix[n,m]
+#    end
+#    println()
+#end
+
 for n = 1:ntime
-    conv_ret_lmix(n, AB.lmix, A.ret, A.ret, B.lmix, B.lmix, I, h)
     conv_lmix_mat(n, AB.lmix, A.lmix, B.mat, I, beta, A.sign)
+    conv_ret_lmix(n, AB.lmix, A.ret, A.ret, B.lmix, B.lmix, I, h)
     for m = 1:ntau
         @show n, m, AB.lmix[n,m]
     end
