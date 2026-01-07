b23d8e44bf8cef7bc68a3478b9fde721613d80f5
Li Huang <lihuang.dmft@gmail.com>
2026-01-07 23:39:09 +0800

    add new convolution_time_step()

--- src/convolution.jl
+++ src/convolution.jl
@@ -292,6 +292,43 @@ function convolution_time_step(
 end
 
 """
+    convolution_time_step(
+        n::I64,
+        C::ℱ{T}, A::ℱ{T}, B::ℱ{T},
+        order::I64,
+        beta::F64,
+        h::F64
+    ) where {T}
+
+To calculate convolution of two contour-ordered Green's functions, i.e.,
+`C(t,t') = A(t,t'') ∗ B(t'',t)` at a given time step `t = nh`.
+
+### Arguments
+* n -> Index of given time step.
+* A -> Contour-ordered Green's function, A(t,t'').
+* B -> Contour-ordered Green's function, B(t'',t').
+* order -> Order for numerical integration.
+* beta -> Inverse temperature, β.
+* h -> Time step interval.
+
+### Returns
+* C -> Contour-ordered Green's function, C(t,t').
+"""
+function convolution_time_step(
+    n::I64,
+    C::ℱ{T}, A::ℱ{T}, B::ℱ{T},
+    order::I64,
+    beta::F64,
+    h::F64
+) where {T}
+    # Sanity check
+    @assert 10 ≥ order ≥ 2
+
+    I = Integrator(order)
+    convolution_time_step(n, C, A, A, B, B, I, beta, h)
+end
+
+"""
     convolution(C, A, f, B)
 
 TO_BE_DONE
