8186296516b91ecb4499a515dcc4c6f1e2e498c6
Li Huang <lihuang.dmft@gmail.com>
2026-01-12 23:17:55 +0800

    fix t_convolution.jl

--- test/basic/t_convolution.jl
+++ test/basic/t_convolution.jl
@@ -53,3 +53,57 @@
     end
     @test err < ϵ
 end
+
+@testset verbose = true "KadanoffBaym: convolution.jl (matrix form)" begin
+    ntime = 15
+    ntau = 501
+    ndim1 = 2
+    ndim2 = 2
+    tmax = 2.0
+    beta = 0.1
+    #
+    δt = 0.01
+    μ = 0.0
+    order = 5
+    ϵ = 1.0e-6
+    wa = 1.123
+    wb = 0.345
+    #
+    for n = 1:1
+        ntime = 2^(n-1) * 10 + 1
+        C = Cn(ntime, ntau, ndim1, ndim2, tmax, beta)
+        @show n, ntime, C.dt
+        G₁ = ℱ(C, BOSE)
+        G₂ = ℱ(C, BOSE)
+        G₃ = ℱ(C, BOSE)
+        G₄ = ℱ(C, BOSE)
+        #
+        H₁ = fill(zero(C64), ndim1, ndim2)
+        H₂ = fill(zero(C64), ndim1, ndim2)
+        H₁[1,1] = wa
+        H₂[1,1] = wb
+        #
+        init_green!(G₁, H₁, μ, beta, C.dt)
+        init_green!(G₂, H₂, μ, beta, C.dt)
+        #
+        I = Integrator(order)
+        #
+        # Prepare exact solution
+        fac = 1.0 / (wa - wb)
+        G₃ = deepcopy(G₁)
+        for tstp = 0:ntime
+            incr!(G₃, G₂, tstp, -1.0)
+            smul!(G₃, tstp, fac)
+        end
+        #
+        # Evaluate convolution
+        convolution(G₄, G₁, G₂, order, beta, C.dt)
+        #
+        # Compute final error
+        err = 0.0
+        for tstp = 0:ntime
+            err = err + distance(G₄, G₃, tstp)
+        end
+        @show err
+    end
+end
