3a73659e4db4d0fedafd8a65a4fa65be13ffd566
Li Huang <lihuang.dmft@gmail.com>
2026-01-14 23:47:59 +0800

    add t_convolution_new.jl

---
+++ test/basic/t_convolution_new.jl
@@ -0,0 +1,21 @@
+    #for m = 1:ntau
+    #    @show m, G₃_₁₁.mat[m][1,1] - G₄.mat[m][1,1]
+    #end
+
+    #for n = 1:ntime
+    #    for m = 1:n
+    #        @show n, m, G₃_₁₁.ret[n,m][1,1] - G₄.ret[n,m][1,1]
+    #    end
+    #end
+
+    #for n = 1:ntime
+    #    for m = 1:ntau
+    #        @show n, m, G₃_₁₁.lmix[n,m][1,1] - G₄.lmix[n,m][1,1]
+    #    end
+    #end
+
+    #for n = 1:ntime
+    #    for m = 1:n
+    #        @show n, m, G₃_₁₁.less[m,n][1,1] - G₄.less[m,n][1,1]
+    #    end
+    #end
