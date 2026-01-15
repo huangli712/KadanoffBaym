31aea54ec88e78fe928ac2a082ed8a284f225d35
Li Huang <lihuang.dmft@gmail.com>
2026-01-15 23:47:59 +0800

    add convolution_time_step()

--- src/convolution.jl
+++ src/convolution.jl
@@ -723,6 +723,41 @@ function convolution_time_step(
     convolution_time_step(n, C, A, Acc, fₜ, B, Bcc, I, beta, h)
 end
 
+"""
+    convolution_time_step(
+        n::I64,
+        C::ℱ{T},
+        A::ℱ{T}, Acc::ℱ{T},
+        fₜ::VecArray{T},
+        B::ℱ{T}, Bcc::ℱ{T},
+        order::I64,
+        beta::F64,
+        h::F64
+    )
+
+Try to calculate convolution of two contour-ordered Green's functions
+(A and B) and a time-dependent function fₜ at a given time step `t = nh`.
+In other words, it implements `C(t,t') = A(t,t'') ∗ f(t'') ∗ B(t'',t)`. 
+
+### Arguments
+* n -> Index of given time step.
+* A -> Contour-ordered Green's function, A(t,t'').
+* Acc -> Complex conjugate to A.
+* fₜ -> Values of time-dependent function, f(t'').
+* B -> Contour-ordered Green's function, B(t'',t').
+* Bcc -> Complex conjugate to B.
+* order -> Order for numerical integration.
+* beta -> Inverse temperature, β.
+* h -> Time step interval.
+
+### Returns
+* C -> Contour-ordered Green's function, C(t,t').
+
+### Notes
+The Matsubara component of C will not be modified in this function.
+
+See also: [`conv_mat`](@ref).
+"""
 function convolution_time_step(
     n::I64,
     C::ℱ{T}, A::ℱ{T}, fₜ::VecArray{T}, B::ℱ{T},
